dab973d19bca3c56a4d842569651066646bf762d
Alfaafan Yusufa Achmad <yusufa0509@gmail.com>
2023-01-20 22:00:43 +0700

    added static folder + views

--- index.js
+++ index.js
@@ -7,6 +7,32 @@ app.use(express.json());
 
 app.set("view engine", "ejs");
 
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/game", (req, res) => {
+  res.render("game");
+});
+
+const authRouter = require("./routes/auth");
+app.use("/auth", authRouter);
+
+// internal server handler
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    status: "fail",
+    error: err.message,
+  });
+});
+
+// 404 handler
+app.use((req, res, next) => {
+  res.status(404).render("404");
+});
+
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
